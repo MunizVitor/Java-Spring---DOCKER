d282709bb70a15c8adc90cd51ef6a3d494ee052c
MunizVitor <vmuniz.dev@gmail.com>
2026-05-30 17:16:00 -0300

    add primary migration in project -- event

---
+++ src/main/resources/db/migration/V1.1__insert_initial_data__by__event.sql
@@ -0,0 +1,5 @@
+INSERT INTO event (title, description, img_url, event_url, date, remote)
+VALUES
+('Spring Boot Workshop', 'Workshop prático sobre Spring Boot e Docker', 'spring.png', 'http://meusite.com/workshop', '2026-06-15 19:00:00', TRUE),
+('Tech Conference', 'Conferência de tecnologia com palestras e networking', 'conference.png', 'http://meusite.com/conference', '2026-07-01 09:00:00', FALSE),
+('Hackathon', 'Competição de programação intensiva de 48 horas', 'hackathon.png', 'http://meusite.com/hackathon', '2026-08-20 10:00:00', TRUE);
